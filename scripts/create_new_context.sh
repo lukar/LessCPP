a2e4986f181ebaf70d95e79f1196895de9868fa1
AvianY <ayelite@gmail.com>
2021-01-23 14:44:31 +0000

    Add a script that generates new context source and header files

---
+++ scripts/create_new_context.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+
+echo "Insert the context name: "
+
+read context_name
+
+alluppercase=${context_name^^}
+definename=${alluppercase// /_}_CONTEXT_LESSCPP
+
+tmp=( $context_name )
+fst_capitalized=${tmp[@]^}
+classname=${fst_capitalized// /}Context
+
+alllowercase=${context_name,,}
+filename=${alllowercase// /_}
+
+cat >${filename}.h << EOF
+#ifndef ${definename}
+#define ${definename}
+
+#include "context.h"
+
+class ${classname} : public Context {
+public:
+	${classname}();
+
+	void update(const float dt, const sf::Vector2f & mousepos) override { m_dt = dt; m_mousepos = mousepos; };
+	Context* processEvent(const sf::Event &) override;
+	Context* processBackgroundTask() override;
+	sf::Texture render() override;
+};
+
+
+#endif // ${definename}
+EOF
+
+cat >${filename}.cc << EOF
+#include "${filename}.h"
+
+${classname}::${classname}() {}
+
+Context* ${classname}::processBackgroundTask() { return nullptr; }
+
+Context* ${classname}::processEvent(const sf::Event & event)
+{
+	return nullptr;
+}
+
+sf::Texture ${classname}::render()
+{
+	rentex.clear();
+
+	rentex.display();
+
+	return rentex.getTexture();
+}
+EOF
